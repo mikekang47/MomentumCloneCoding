2df3cfeadc420118a3fee849545de566b6c4e4f8
mikekang47 <gyeong0308@gmail.com>
2021-09-06 01:59:18 +0900

    Modify typo

--- docs/js/background.js
+++ docs/js/background.js
@@ -4,7 +4,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
 
