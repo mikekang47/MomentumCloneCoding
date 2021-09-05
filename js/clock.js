188dbd5a324f2f8455389d95cc2f4aa7822e6a45
mikekang47 <gyeong0308@gmail.com>
2021-09-06 01:44:51 +0900

    Complete to deal with html

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+
+const clock = document.querySelector("div#clock");
+
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    const second = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours} : ${minute} : ${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000);  
